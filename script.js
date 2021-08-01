0ecae1cf4b7cf2020b12a72e353d549301cd06e9
Aslak Aarflot Jønsson <60319109+aslakaaj@users.noreply.github.com>
2021-08-01 19:34:31 +0200

    Add files via upload
    Added a sepperate file, so I don't code my javascript inside the HTML tags anymore

---
+++ script.js
@@ -0,0 +1,65 @@
+window.onload = oppstart;
+            
+var toContact;
+var navStalk;
+var rotatingImg;
+var skillContainer;
+
+function oppstart()
+{
+    document.getElementById("divAbout").style.animation = "fadeIn 1s alternate";
+    document.getElementById("divAbout").style.opacity = "1";
+
+    navStalk = document.getElementById("getInTouch");
+    navStalk.onclick = seeMore;
+
+    skillContainer = document.getElementById("skill-container");
+    skillContainer.onmouseenter = enlargeContainer;
+    skillContainer.onmouseleave = resetContainer;
+
+    // rotatingImg = document.getElementById("rotating-img");
+    // window.onscroll = rotateImg;
+
+    toContact = document.getElementById("skip-to-contact");
+    toContact.onmouseenter = changeContactText;
+    toContact.onmouseleave = changeBackContactText;
+    toContact.onclick = seeMore;
+    // HoverOver();
+}
+
+function enlargeContainer( )
+{
+    skillContainer.style.animation = "enlargeContainer 0.6s normal";
+    skillContainer.style.animationFillMode = "forwards";
+}
+
+function resetContainer()
+{
+    skillContainer.style.animation = "enlargeContainer 0.6s reverse";
+    skillContainer.style.animationFillMode = "forwards";
+}
+
+function rotateImg()
+{
+    rotatingImg.style.transform = "rotate(90deg)";
+}
+
+function seeMore()
+{
+    var element = document.querySelector("#stalk-section");
+
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function changeContactText()
+{
+    toContact.innerHTML = "📭 Get in touch";
+    toContact.style.cursor = "pointer";
+    toContact.style.color = "white";
+}
+
+function changeBackContactText()
+{
+    toContact.innerHTML = "📫 Get in touch";
+    toContact.style.color = "rgb(255, 86, 142)";
+}
